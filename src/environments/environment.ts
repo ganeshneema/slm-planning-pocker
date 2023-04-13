4309e99d8013d2a395caa308b25128d60e9090c7
pxwu1m <ganesh.neema@siemens.com>
2023-04-13 12:57:19 +0530

    update project name

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,23 +4,23 @@
 
 export const environment = {
   firebase: {
-    projectId: 'coco-6c8a7',
+    projectId: 'slm-planning-poker',
     appId: '1:1091082167973:web:b429f13a0db7c170668f4c',
-    storageBucket: 'coco-6c8a7.appspot.com',
+    storageBucket: 'slm-planning-poker.appspot.com',
     locationId: 'asia-southeast2',
     apiKey: 'AIzaSyAfoY5Dve1Yp8Ho8GvVmU8LZwhTzauYR68',
-    authDomain: 'coco-6c8a7.firebaseapp.com',
+    authDomain: 'slm-planning-poker.firebaseapp.com',
     messagingSenderId: '1091082167973',
     measurementId: 'G-P4TS40EDVY',
   },
   production: false,
-  apiKey: "AIzaSyAfoY5Dve1Yp8Ho8GvVmU8LZwhTzauYR68",
-  authDomain: "coco-6c8a7.firebaseapp.com",
-  projectId: "coco-6c8a7",
-  storageBucket: "coco-6c8a7.appspot.com",
-  messagingSenderId: "1091082167973",
-  appId: "1:1091082167973:web:b429f13a0db7c170668f4c",
-  measurementId: "G-P4TS40EDVY"
+  apiKey: 'AIzaSyAfoY5Dve1Yp8Ho8GvVmU8LZwhTzauYR68',
+  authDomain: 'slm-planning-poker.firebaseapp.com',
+  projectId: 'slm-planning-poker',
+  storageBucket: 'slm-planning-poker.appspot.com',
+  messagingSenderId: '1091082167973',
+  appId: '1:1091082167973:web:b429f13a0db7c170668f4c',
+  measurementId: 'G-P4TS40EDVY',
 };
 
 /*
